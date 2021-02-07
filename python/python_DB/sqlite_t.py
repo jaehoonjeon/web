7f11bd4bad235a1a0d19f37a8b4710d464b05b8c
jhjeon <jajeon0020@gmail.com>
2021-02-07 21:58:39 +0900

    sqlite

--- python/python_DB/sqlite_t.py
+++ python/python_DB/sqlite_t.py
@@ -10,24 +10,25 @@ class Port:
                 if hasattr(self, k): 
                     setattr(self, k, v)
 
-def getKor(obj):
-    if type(obj) == str:
-        return (obj.encode('ISO-8859-1')).decode('euc-kr')
-    return obj
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
 
 def getData(query):
     returnData = []
     try:
         conn = sqlite3.connect('Ticket30DB.db')
+        conn.row_factory = dict_factory
         cursor = conn.cursor()
         cursor.execute(query)
+        
+        returnData = []
         for row in cursor:
-            print(row)
-
-        #for row in cursor:
-            #returnData.append(row)
-
-        #return returnData
+            returnData.append(Port(row))
+        return returnData
+            
     except Exception as e:
         print('Exception :: {}'.format(e))
     finally:
@@ -37,3 +38,8 @@ def getData(query):
 
 query = ' SELECT * FROM Port Limit 10 '
 returnData = getData(query)
+printData = ''
+for obj in returnData:
+    printData = printData + obj.Port  + ',' + obj.PortID + '\t'
+
+print(printData + '\n\n\n')
